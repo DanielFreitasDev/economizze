c5d30b1500ef4a66215cd20d3da9fa7859e279f3
Daniel Freitas <danielsfreitas97@gmail.com>
2026-02-26 14:47:46 -0300

    Commit inicial

---
+++ src/main/java/io/nexus/economizze/EconomizzeApplication.java
@@ -0,0 +1,13 @@
+package io.nexus.economizze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomizzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomizzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nexus/economizze/EconomizzeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nexus.economizze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomizzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
